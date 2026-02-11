83f72b8a324f1292916bf6057cd72631b1c51362
Bipasha Halder <your.email@example.com>
2026-02-11 13:06:07 +0530

    Design Sync: Update index, navbar, footer, tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,8 @@ module.exports = {
       colors: {
         primary: '#2c3e50',
         accent: '#f4d03f',
-        pale: '#ffeeb0',
-        darkgold: '#fcd670',
+        'pale-yellow': '#ffeeb0',
+        'dark-gold': '#fcd670',
       },
       fontFamily: {
         sans: ['Inter', 'Open Sans', 'sans-serif'],
